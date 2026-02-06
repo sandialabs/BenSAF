c126421e277112251530dd3892dbbeafda8b5830
kbonney <kirkb1998@gmail.com>
2026-02-06 10:58:05 -0500

    update init

--- bensaf/__init__.py
+++ bensaf/__init__.py
@@ -4,5 +4,6 @@ SAF Toolkit - A toolkit for sustainable aviation fuel analysis.
 
 import bensaf.utils as utils
 import bensaf.workflow as workflow
+import bensaf.aermod_parser as aermod_parser
 
 __version__ = "0.1.0"
